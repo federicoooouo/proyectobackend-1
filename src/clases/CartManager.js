0bba50f9dc27abfb0875f6164ce63cff9f2ad2d3
federico-murua <federicog.murua12@gmail.com>
2025-03-18 18:43:33 -0300

    terminando proyecto final

--- src/clases/CartManager.js
+++ src/clases/CartManager.js
@@ -1,61 +1,23 @@
-import fs from "fs"
 import { cartModel } from "../models/cart.model.js";
 
 class CartManager{
     constructor(){
-       /* this.carts = [],
-        this.file = "carrito.json",
-        this.creatFile()*/
+
     }
-
-   /* creatFile(){
-        if(!fs.existsSync(this.file))
-            fs.writeFileSync(this.file, JSON.stringify(this.carts))
-        }*/
-
-/*getID(){
-    this.getCarts();
-    let max = 0;
-    this.carts.forEach(item => {
-        if(item.id > max){
-            max = item.id;
-        }
-    })
-    return max + 1;
-    }  */
     async getCarts(){
-        /*this.carts = JSON.parse(fs.readFileSync(this.file, "utf-8"));
-        return this.carts;*/
+
         return await cartModel.find().lean();
     }
     async getCartById(id){
         return await cartModel.find({_id:id}).lean();
-        /*  this.getCarts();
-        let cart = this.carts.find(item => item.id == id);
-        return cart ? cart.products : {"error": "No se encontro el carrito"};*/
     }    
 
     async createCart() {
         await cartModel.create({product:[]});
-    
-        /* const cart = {id:this.getID(), products:[]};
-        this.carts.push(cart);
-        this.saveCarts();*/
         }
     
 
     async addCartProduct(cid, pid) {
-        /*  this.getCarts();
-            let cart = this.carts.find(item => item.id == cid);
-            let product = cart.products.find(item => item.product == pid);
-    
-            if(product){
-                product.quantity += 1;
-            } else {
-                let product = {product:pid, quantity:1};
-                cart.products.push(product);
-            }
-            this.saveCarts(); */
             let cart = await cartModel.findOne({_id:cid}).lean();
             let product = cart.products.find(item => item.product == pid);
     
@@ -67,9 +29,6 @@ class CartManager{
             }
         await cartModel.updateOne({_id:cid}, {products: cart.products});
         }
-    /*saveCarts(){
-        fs.writeFileSync(this.file, JSON.stringify(this.carts));
-    }  */  
         }
         
     export default CartManager
--- src/clases/ProductManager.js
+++ src/clases/ProductManager.js
@@ -2,27 +2,7 @@ import { productsModel } from '../models/products.model.js';
 
 
 class ProductManager{
-/*  constructor(){
-        this.products = []
-       // this.file = "productos.json",
-        //this.creatFile()
-    } */
 
-/*    creatFile(){
-        if(!fs.existsSync(this.file))
-            fs.writeFileSync(this.file, JSON.stringify(this.products))
-    }*/
-
-/*   getID(){
-        this.getProducts();
-        let max = 0;
-        this.products.forEach(item => {
-            if(item.id > max){
-                max = item.id;
-            }
-        })
-        return max + 1;
-    }*/
 async getProducts(limit, page, query, sort){
     try {
 
@@ -50,45 +30,26 @@ async getProducts(limit, page, query, sort){
 
     }
 
-    //this.products = JSON.parse(fs.readFileSync(this.file, "utf-8"));
-        //this.products =  await productsModel.find();
     }
     async getProductById(id){
-        //this.getProducts();
+    
         let product = await productsModel.find({_id:id});
         return product ? product : {"error": "Product not foundd"};
     }
     async addProduct(product){
-        /*this.getProducts();
-        let newProduct = {id:this.getID(), ...product};
-        this.products.push(newProduct);
-        this.saveProducts();*/
+
     await productsModel.create({...product})
     }
     async editProduct(id, product){
     
-    /* this.getProducts();
-        let actualProduct = this.products.find(item => item.id == id);
-        actualProduct.title = product.title;
-        actualProduct.description = product.description;
-        actualProduct.code = product.code;
-        actualProduct.price = product.price;
-        actualProduct.status = product.status;
-        actualProduct.category = product.category;
-        actualProduct.thumbnails = product.thumbnails;
-        this.saveProducts();*/
         await productsModel.updateOne({_id:id}, {...product});
 
     }
     async deleteProduct(id){
         await productsModel.deleteOne({_id:id});
-        /* this.getProducts();
-        this.products = this.products.filter(item => item.id != id);
-        this.saveProducts();*/
+
     }
-/* saveProducts(){
-        fs.writeFileSync(this.file, JSON.stringify(this.products));
-    }*/
+
 }
 
 export default ProductManager
